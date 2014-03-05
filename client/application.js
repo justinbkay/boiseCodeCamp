d616c5533e026e019074bc81e18ee6b27f620259
Justin <justinbkay@gmail.com>
2014-03-05 15:25:05 -0700

    adding routing and a profile

--- client/application.js
+++ client/application.js
@@ -1,4 +1,4 @@
-Template.content.users = function () {
+Template.home.users = function () {
   if (Meteor.user()) {
     var id = Meteor.user()._id;
     return Meteor.users.find({ _id: {$ne: id } } , {sort: {"profile.name": 1}});
@@ -6,3 +6,19 @@ Template.content.users = function () {
     return Meteor.users.find();
   }
 };
+
+Template.profile.helpers({
+  name: function() {
+    Meteor.user().profile.name;
+  }
+});
+
+Template.profile.events({
+  'click #save': function(e) {
+    var name = $('#name').val();
+    var tagline = $('#tagline').val();
+    Meteor.users.update(Meteor.user()._id, {$set: {"profile.name": name, "profile.tagline": tagline}});
+    Router.go('home');
+  }
+});
+
---
+++ client/router.js
@@ -0,0 +1,26 @@
+var IR_BeforeHooks = {
+  isLoggedIn: function() {
+    if (!(Meteor.loggingIn() || Meteor.user())) {
+      Router.go('home');
+      this.stop();
+    }
+  }
+};
+
+Router.configure({
+    layoutTemplate: 'layout',
+    loadingTemplate: 'loading',
+    notFoundTemplate: 'notFound'
+});
+
+Router.before(IR_BeforeHooks.isLoggedIn, {only: ['profile']});
+
+Router.map(function () {
+    this.route('home', {
+      path: '/'
+    });
+
+    this.route('profile', {
+      path: '/profile'
+    });
+});
