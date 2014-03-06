316079a92f17685f6a1d80516dcf5903d0f7d382
Justin <justinbkay@gmail.com>
2014-03-05 21:40:59 -0700

    arranging the files and adding a link to the profile

--- client/application.js
+++ client/application.js
@@ -7,18 +7,21 @@ Template.home.users = function () {
   }
 };
 
-Template.profile.helpers({
-  name: function() {
-    Meteor.user().profile.name;
-  }
-});
-
 Template.profile.events({
   'click #save': function(e) {
     var name = $('#name').val();
     var tagline = $('#tagline').val();
     Meteor.users.update(Meteor.user()._id, {$set: {"profile.name": name, "profile.tagline": tagline}});
     Router.go('home');
+  },
+
+  'click #cancel': function() {
+    Router.go('home');
   }
 });
 
+Template.layout.events({
+  'click #profile': function(e) {
+    Router.go('profile');
+  }
+});
