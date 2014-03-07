97a8ef628bd650373a08bcf02d34006b12f1577e
Justin <justinbkay@gmail.com>
2014-03-06 20:01:47 -0700

    sorting the users when not logged in

--- client/application.js
+++ client/application.js
@@ -3,7 +3,7 @@ Template.home.users = function () {
     var id = Meteor.user()._id;
     return Meteor.users.find({ _id: {$ne: id } } , {sort: {"profile.name": 1}});
   } else {
-    return Meteor.users.find();
+    return Meteor.users.find({}, {sort: {"profile.name": 1}});
   }
 };
 
@@ -11,8 +11,13 @@ Template.profile.events({
   'click #save': function(e) {
     var name = $('#name').val();
     var tagline = $('#tagline').val();
-    Meteor.users.update(Meteor.user()._id, {$set: {"profile.name": name, "profile.tagline": tagline}});
-    Router.go('home');
+    if (name.length > 0) {
+      Meteor.users.update(Meteor.user()._id, {$set: {"profile.name": name, "profile.tagline": tagline}});
+      Router.go('home');
+    } else {
+      $('.bg-danger').show();
+      $('#name_group').addClass("has-error");
+    }
   },
 
   'click #cancel': function() {
